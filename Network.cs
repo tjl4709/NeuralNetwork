50ab7de4f8f58ced08e78d2c8e6e467fe433da0a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 20:06:18 -0400

    Started framework for neural network

---
+++ Network.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Network
+{
+	private Layer[] layers;
+
+	public Network(int[] layerSizes)
+	{
+		layers = new Layer[layerSizes.Length - 1];
+		for (int i = 0; i < layers.Length; i++)
+			layers[i] = new Layer(layerSizes[i], layerSizes[i + 1]);
+	}
+}
+
+public class Layer
+{
+	private double[,] weights;
+	public double[,] Weights { get { return weights; } }
+
+	private double[] biases;
+	public double[] Biases { get { return biases; } }
+
+	public Layer(int preSize, int size)
+	{
+		weights = new double[size, preSize];
+		biases = new double[size];
+		for (int r = 0; r < weights.GetLength(0); r++)
+		{
+			biases[r] = 1;//random number (0,1]
+			for (int c = 0; c < weights.GetLength(1); c++)
+				weights[r,c] = 1;//generate random numbers on normal distribution
+		}
+	}
+}
